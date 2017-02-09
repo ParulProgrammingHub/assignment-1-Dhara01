a17620df3f2af5be4451dddd74237f9092309a27
Dhara01 <150303105106@paruluniversity.ac.in>
2017-02-09 20:30:56 +0530

    Create Program-11.py

---
+++ Program-11.py
@@ -0,0 +1,8 @@
+p=float(raw_input(“Enter principle:”))
+i=float (raw_input(“Enter the percentage of interest rate: “))
+t=float(raw_input(“Enter time (in years): “))
+n=float(input(“Enter no. Of times interest is compounded: “))
+def compound_interest(principle,rate,time,node):
+               A = p * ( (i+r)/n)  ** (n*t) )
+               print “Intereast is “,A
+compound_interest(principle=p,rate=r,time=t,node=n)
