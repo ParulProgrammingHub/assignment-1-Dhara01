6d89ae1048c3c2baaf5c3149f39f7df50d482028
Dhara01 <150303105106@paruluniversity.ac.in>
2017-02-09 20:03:23 +0530

    Create Program-1.py

---
+++ Program-1.py
@@ -0,0 +1,6 @@
+length=int(input(“Enter the length: “))
+breadth=int(input(“Enter the breadth: “))
+area = length * breadth
+perimeter =2 * ( length + breadth )
+print (“Area is “,’area’)
+print (“Perimeter is  “,’perimeter’)
